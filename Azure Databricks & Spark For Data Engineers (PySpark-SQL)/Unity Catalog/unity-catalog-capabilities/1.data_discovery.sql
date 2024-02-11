1b125441cd0b0b4932f216d9bea6b4e605f1a75c
vajay8679 <vajay8679@gmail.com>
2024-02-11 18:00:57 +0530

    all

---
+++ Azure Databricks & Spark For Data Engineers (PySpark-SQL)/Unity Catalog/unity-catalog-capabilities/1.data_discovery.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+select * from system.information_schema.tables where table_name = 'results';
+
+-- COMMAND ----------
+
+select * from formula1_dev.information_schema.tables where table_name = 'results';
+
+-- COMMAND ----------
+
+
